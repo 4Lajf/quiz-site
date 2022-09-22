c72afeb539ff668fadba0de756c5d8ad90d9084d
4Lajf <szymonkoziarz12@gmail.com>
2022-09-22 17:58:58 +0200

    edge functions testing

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import sveltePreprocess from 'svelte-preprocess'
 import netlify from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
-    preprocess: sveltePreprocess(),
     kit: {
-        // By default, `npm run build` will create a standard Node app.
-        // You can create optimized builds for different platforms by
-        // specifying a different adapter
         adapter: netlify({
             edge: true
         })
-    },
-}
+    }
+};
+
 export default config;
