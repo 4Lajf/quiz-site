6c6f01ee632af01773d6cbf7d6097050cd93f750
4Lajf <szymonkoziarz12@gmail.com>
2022-11-16 21:55:31 +0100

    add chat room

---
+++ src/routes/tsuru/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<main>
+	<section
+		class="flex flex-col justify-center w-full h-screen m-0 text-gray-100 bg-gray-800 homescreen "
+	>
+		<div class="container flex flex-col items-center justify-center px-5 py-24 mx-auto">
+			<div class="w-full text-center lg:w-2/3">
+				<h1 class="mb-4 font-mono text-3xl font-medium text-gray-300 title-font sm:text-4xl">
+					Tsuru 2022 - Chat Room
+				</h1>
+				<widgetbot
+					server="1042541017909432420"
+					channel="1042541020052734024"
+					width="800"
+					height="600"
+				/>
+				<script src="https://cdn.jsdelivr.net/npm/@widgetbot/html-embed"></script>
+			</div>
+		</div>
+	</section>
+</main>
